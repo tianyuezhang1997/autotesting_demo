b23b4d4780a9b364b274f5d25d6c12ad3e531e7b
tianyuezhang1997 <tianyuezhang1997@gmail.com>
2021-10-31 08:49:24 -0700

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -3,7 +3,7 @@
 // Here is where the recipes that you will fetch.
 // Feel free to add your own here for part 2, if they are local files simply add their path as a string.
 const recipes = [
-  'https://introweb.tech/assets/json/ghostCookies.json',
+  'https://introweb.tech/assets/json/ghostCookies.jso',
   'https://introweb.tech/assets/json/birthdayCake.json',
   'https://introweb.tech/assets/json/chocolateChip.json'
 ];
@@ -22,7 +22,6 @@ async function init() {
   // if they didn't successfully load, quit the function
   if (!fetchSuccessful) {
     console.log('Recipe fetch unsuccessful');
-    return;
   };
   // Add the first three recipe cards to the page
   createRecipeCards();
