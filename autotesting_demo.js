6860c699ac8853429896b084d1edf80303541f11
tianyuezhang1997 <55299504+tianyuezhang1997@users.noreply.github.com>
2021-11-01 13:49:34 -0700

    Update autotesting_demo.js

--- autotesting_demo.js
+++ autotesting_demo.js
@@ -1,9 +1,9 @@
 import { Selector } from 'testcafe';
 
-fixture `New Fixture`
+fixture `Latest Fixture`
     .page `https://tianyuezhang1997.github.io/autotesting_demo/index.html`;
     
-test(`Updated Test`, async t => {
+test(`Latest Test`, async t => {
     await t
         .click(Selector('#button-wrapper button').withText('Show more'))    
         .click(Selector('#button-wrapper button').withText('Show less'))
@@ -19,5 +19,17 @@ test(`Updated Test`, async t => {
         .click('main [name="horn"]')
         .click(Selector('main option').withText('Party Horn'))
         .click(Selector('main button').withText('Play Sound'))
+        .click(Selector('footer a').withText('Recipe Demo'))
+    await t
+        .click(Selector('footer a').withText('Explore - Speech Synthesis'))
+        .typeText('main [name="text-to-speak"]', 'This is a testing message')
+        .click('main [name="voice"]')
+        .click(Selector('main option').withText('Google 日本語 (ja-JP)'))
+        .click(Selector('main button').withText('Press to Talk'))
+        .click(Selector('footer a').withText('Recipe Demo'))
+    await t
+        .click(Selector('#button-wrapper button').withText('Show more'))
+        .click(Selector('#button-wrapper button').withText('Show less'))
+        .click(Selector('footer a').withText('Explore - Speech Synthesis'))
         .click(Selector('footer a').withText('Recipe Demo'))
 });
