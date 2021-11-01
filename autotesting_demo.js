a5a916c399da69e1401f0c742bcfdf1ff16b7faa
tianyuezhang1997 <tianyuezhang1997@gmail.com>
2021-11-01 08:31:21 -0700

    Update autotesting_demo.js

--- autotesting_demo.js
+++ autotesting_demo.js
@@ -3,7 +3,7 @@ import { Selector } from 'testcafe';
 fixture `New Fixture`
     .page `https://tianyuezhang1997.github.io/autotesting_demo/index.html`;
     
-test(`New Test`, async t => {
+test(`Updated Test`, async t => {
     await t
         .click(Selector('#button-wrapper button').withText('Show more'))    
         .click(Selector('#button-wrapper button').withText('Show less'))
